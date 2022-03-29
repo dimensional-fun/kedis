b5cd4f7a0d92384482e12fda43c65bf22c61aea0
melike2d <ml2dofficial@gmail.com>
2022-03-29 02:30:05 -0700

    tests(core): add basic benchmarking code to test against jedis

--- kedis-core/build.gradle.kts
+++ kedis-core/build.gradle.kts
@@ -20,6 +20,8 @@ dependencies {
     // tests
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testImplementation("redis.clients:jedis:4.2.0")
+
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
     testRuntimeOnly("org.slf4j:slf4j-simple:1.7.30")
--- kedis-core/src/test/kotlin/main.kt
+++ kedis-core/src/test/kotlin/main.kt
@@ -1,24 +1,8 @@
 import mixtape.oss.kedis.RedisClient
-import mixtape.oss.kedis.command.RedisCommand
 import mixtape.oss.kedis.command.RedisCommands
-import mixtape.oss.kedis.command.RedisTypeReader
-import mixtape.oss.kedis.util.ping
-import kotlin.time.ExperimentalTime
-import kotlin.time.measureTimedValue
 
-@OptIn(ExperimentalTime::class)
 suspend fun main() {
-    val client = RedisClient("redis://:password@127.0.0.1")
+    val kedis = RedisClient("redis://:password@localhost")
 
-    println(client.sendCommand(RedisCommand("KEYS", RedisTypeReader.StringList, "*")))
-
-    val info = client.sendCommand(RedisCommands.info)!!
-        .lines()
-        .filterNot { it.startsWith("#") || it.isBlank() }
-        .map { it.split(':') }
-        .associate { it[0] to it[1] }
-
-    println(info)
-
-    println(measureTimedValue { client.ping() })
+    println(kedis.executeCommand(RedisCommands.aclHelp())?.withIndex()?.joinToString("\n") { (i, v) -> "${i + 1}) $v" })
 }
